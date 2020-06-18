3062971201db0887d451c03b051e1e7834454f15
bmai <bmai@sfu.ca>
2020-06-17 18:13:47 -0700

    working on comments

---
+++ src/Comment.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './Comment.css'
+
+
+
+const Comment = (props) => {
+
+    const [commentLink, setCommentLink] = useState("")
+
+    const data = props.data
+
+    let commentDate = new Date(data.author_created_utc * 1000)
+    commentDate = commentDate.getFullYear().toString() + '/' + commentDate.getMonth().toString() + '/' + commentDate.getDate().toString()
+
+    useEffect(() => {
+        axios.get(`https://api.pushshift.io/reddit/search/submission/?ids=${data.link_id}`)
+            .then((response) => {
+                setCommentLink(response.data.data[0].full_link + data.id)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
+    return (
+        <div className="Comment">
+            <p>
+                <span className="Author">
+                    by <a href={`https://www.reddit.com/u/${data.author}`}>{data.author}</a>
+                </span>
+                <span> </span>
+                <span className="Subreddit">
+                    to <a href={`https://www.reddit.com/r/${data.subreddit}`}>r/{data.subreddit}</a>
+                </span>
+            </p>
+            <p>{data.body}</p>
+            <p className="Date">Posted on {commentDate.toString()}</p>
+            <a href={commentLink}>link</a>
+        </div>
+    )
+}
+
+export default Comment
--- src/Form.js
+++ src/Form.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import loading from './loading.svg'
 import Post from './Post'
+import Comment from './Comment'
 
 const Form = () => {
+
+    const [searchSubmissions, setSearchSubmissions] = useState(true)
+    const [searchComments, setSearchComments] = useState(false)
 
     const [author, setAuthor] = useState("")
     const [title, setTitle] = useState("")
@@ -12,6 +16,12 @@ const Form = () => {
     const [size, setSize] = useState(50)
     const [after, setAfter] = useState()
     const [before, setBefore] = useState()
+
+    const [sortAsc, setSortAsc] = useState(false)
+    const [sortDesc, setSortDesc] = useState(true)
+    const [sortScore, setSortScore] = useState(true)
+    const [sortCreated, setSortCreated] = useState(false)
+    const [sortNumComments, setSortNumComments] = useState(false)
 
     const [isLoading, setIsLoading] = useState(false)
     const [apiResponse, setAPIResponse] = useState([])
@@ -26,13 +36,59 @@ const Form = () => {
         if (name === "size") { setSize(value) }
         if (name === "after") { setAfter(value) }
         if (name === "before") { setBefore(value) }
+        if (name === "sort") {
+            if (value === "desc") {
+                setSortDesc(true)
+                setSortAsc(false)
+            } else {
+                setSortDesc(false)
+                setSortAsc(true)
+            }
+        }
+
+        if (name === "searchType") {
+            if (value === "submissions") {
+                setSearchSubmissions(true)
+                setSearchComments(false)
+            } else {
+                setSearchSubmissions(false)
+                setSearchComments(true)
+            }
+        }
+
+        if (name === "sortType") {
+            if (value === "score") {
+                setSortScore(true)
+                setSortCreated(false)
+                setSortNumComments(false)
+            }
+            else if (value === "createdDate") {
+                setSortScore(false)
+                setSortCreated(true)
+                setSortNumComments(false)
+            } else {
+                setSortScore(false)
+                setSortCreated(false)
+                setSortNumComments(true)
+            }
+        }
 
     }
 
     const apiQuery = (event) => {
         setIsLoading(true)
         event.preventDefault()
-        axios.get("https://api.pushshift.io/reddit/search/submission", {
+
+        const apiEndPoint = searchSubmissions ?
+            "https://api.pushshift.io/reddit/search/submission" :
+            "https://api.pushshift.io/reddit/search/comment"
+
+        const sortAscOrDesc = sortDesc ? "desc" : "asc"
+        const sortType = sortScore ?
+            "score" :
+            sortNumComments ? "num_comments" : "created_utc"
+
+        axios.get(apiEndPoint, {
             params: {
                 author: author,
                 title: title,
@@ -40,7 +96,9 @@ const Form = () => {
                 subreddit: subreddit,
                 size: size,
                 after: after,
-                before: before
+                before: before,
+                sort: sortAscOrDesc,
+                sort_type: sortType
             }
         })
             .then((response) => {
@@ -54,33 +112,69 @@ const Form = () => {
 
     }
 
-    const apiResponseArray = apiResponse.map(data => <Post key={data.id} data={data} />)
+    const apiResponseArray =    searchSubmissions ?
+                                apiResponse.map(data => <Post key={data.id} data={data} />) :
+                                apiResponse.map(data => <Comment key={data.id} data={data} />)
 
     return (
         <form onSubmit={apiQuery}>
+            <label>Search Type </label>
+            <label>
+                <input type="radio" name="searchType" onChange={handleChange} value="submissions" checked={searchSubmissions} />
+                Posts
+            </label>
+            <label>
+                <input type="radio" name="searchType" onChange={handleChange} value="comments" checked={searchComments} />
+                Comments
+            </label>
+            <br />
             <label>Author </label>
-            <input type="text" name="author" onChange={handleChange} value={author}/>
+            <input type="text" name="author" onChange={handleChange} value={author} />
             <br />
             <label>Subreddit </label>
-            <input type="text" name="subreddit" onChange={handleChange} value={subreddit}/>
+            <input type="text" name="subreddit" onChange={handleChange} value={subreddit} />
             <br />
             <label>Search Term </label>
-            <input type="text" name="searchTerm" onChange={handleChange} value={searchTerm}/>
+            <input type="text" name="searchTerm" onChange={handleChange} value={searchTerm} />
             <br />
             <label>Title </label>
             <input type="text" name="title" onChange={handleChange} value={title} />
             <br />
             <label>Return Size </label>
-            <input type="number" name="size" onChange={handleChange} value={size}/>
+            <input type="number" name="size" onChange={handleChange} value={size} />
             <br />
             <label>After </label>
-            <input type="date" name="after" onChange={handleChange} value={after}/>
+            <input type="date" name="after" onChange={handleChange} value={after} />
             <br />
             <label>Before </label>
-            <input type="date" name="before" onChange={handleChange} value={before}/>
+            <input type="date" name="before" onChange={handleChange} value={before} />
             <br />
+            <label>Sort </label>
+            <label>
+                <input type="radio" name="sort" onChange={handleChange} value="desc" checked={sortDesc} />
+                Descending
+            </label>
+            <label>
+                <input type="radio" name="sort" onChange={handleChange} value="asc" checked={sortAsc} />
+                Ascending
+            </label>
+            <br />
+            <label>Sort Type </label>
+            <label>
+                <input type="radio" name="sortType" onChange={handleChange} value="score" checked={sortScore} />
+                Score
+            </label>
+            <label>
+                <input type="radio" name="sortType" onChange={handleChange} value="createdDate" checked={sortCreated} />
+                Created Date
+            </label>
+            <label>
+                <input type="radio" name="sortType" onChange={handleChange} value="numComments" checked={sortNumComments} />
+                Number of Comments
+            </label>
 
-            <button>Submit</button>
+
+            <button>Search</button>
             <br />
             <div>
                 {isLoading ? <img src={loading} alt="loading"></img> : apiResponseArray}
