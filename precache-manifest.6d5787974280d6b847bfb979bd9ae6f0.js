06f5e23b1a2a45d1677bfad5656957c3d5ffb85d
bmai <bmai@sfu.ca>
2020-09-05 03:44:15 -0700

    Updates

--- precache-manifest.6d5787974280d6b847bfb979bd9ae6f0.js
+++ precache-manifest.6d5787974280d6b847bfb979bd9ae6f0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d84bbaf5f7e9a72edcb2c82034f602fb",
+    "revision": "c7355437b18cfa1d8ff4dd488537b140",
     "url": "/reddit-search/index.html"
   },
   {
-    "revision": "8c5be362e39433f6ce91",
+    "revision": "ada9dd86f937e6091dca",
     "url": "/reddit-search/static/css/main.9705a9b6.chunk.css"
   },
   {
-    "revision": "e7a8e70c9e416bceb62f",
-    "url": "/reddit-search/static/js/2.cb4d2842.chunk.js"
+    "revision": "a3c54e487d5e03d09490",
+    "url": "/reddit-search/static/js/2.37ea6594.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/reddit-search/static/js/2.cb4d2842.chunk.js.LICENSE.txt"
+    "url": "/reddit-search/static/js/2.37ea6594.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8c5be362e39433f6ce91",
-    "url": "/reddit-search/static/js/main.137107d5.chunk.js"
+    "revision": "ada9dd86f937e6091dca",
+    "url": "/reddit-search/static/js/main.943ed2f9.chunk.js"
   },
   {
     "revision": "1b5af96df8ce7dc61c15",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reddit-search/precache-manifest.705d6d7d05885b5faa11e8c27a8ca237.js"
+  "/reddit-search/precache-manifest.6d5787974280d6b847bfb979bd9ae6f0.js"
 );
 
 self.addEventListener('message', (event) => {
